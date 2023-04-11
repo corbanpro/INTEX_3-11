63bb08f28934078bcd5f400fb659fa2bc845db83
corbanpro <corbanpro@gmail.com>
2023-04-11 16:44:13 -0600

    Burial View

--- Models/Color.cs
+++ Models/Color.cs
@@ -10,7 +10,7 @@ namespace INTEX_3_11.Models
     public partial class Color
     {
         public long Id { get; set; }
-        public string Value { get; set; }
+        public string? Value { get; set; }
         public int? Colorid { get; set; }
     }
 }
