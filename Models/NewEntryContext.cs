b76d684c0f95c437dee17ce2333e024419b506f9
bopellopa <bellak20@byu.edu>
2023-04-11 16:46:39 -0600

    general outline  for the add data page

---
+++ Models/NewEntryContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace INTEX_3_11.Models
+{
+    public class NewEntryContext : DbContext
+    {
+        public NewEntryContext(DbContextOptions<NewEntryContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<NewEntry> Responses { get; set; }
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+        }
+
+    }
+}
