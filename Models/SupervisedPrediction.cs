ea1f139fa82c64a0da18ebee067527280a9c851a
Eliot Amado <eamado@byu.edu>
2023-04-12 11:28:38 -0600

    Supervised Prediction Model

---
+++ Models/SupervisedPrediction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace INTEX_3_11.Models
+{
+    public class SupervisedPrediction
+    {
+
+        public double Depth { get; set; } 
+        public double Adultsubadult { get; set; }
+        public double Southtohead { get; set; } 
+        public string Haircolor { get; set; } 
+        public double Westtohead { get; set; }
+        public double Length { get; set; } 
+        public double Westtofeet { get; set; } 
+        public double Southtofeet { get; set; } 
+        public double femurHeadDiameter { get; set; }
+        public double HumerusLength { get; set; }
+        public double TextileFunciton { get; set; }
+
+    }
+}
